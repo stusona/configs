6a9e4acf76a4eab9aea3fca424b58fac000b2c3e
stusona <supadupastu@gmail.com>
2020-01-06 08:35:05 -0800

    added fps to giffer.sh

---
+++ giffer.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# Based on https://engineering.giphy.com/how-to-make-gifs-with-ffmpeg/
+
+echo Input variables:
+echo 1. Name of video file
+echo 2. Time start
+echo 3. Length to convert to gif
+echo 4. Frames per second
+
+# 
+ffmpeg -ss $2 -t $3 -i $1 -filter_complex "[0:v] fps=$4,scale=480:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" output.gif
