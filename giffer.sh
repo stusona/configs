99ad5e73366eb0bdbd02cb845acf08c85671789c
Stu <supadupastu@gmail.com>
2020-04-30 12:02:04 -0700

    added output pixel width option

--- giffer.sh
+++ giffer.sh
@@ -7,6 +7,6 @@ echo 1. Name of video file
 echo 2. Time start
 echo 3. Length to convert to gif
 echo 4. Frames per second
-
+echo 5. Output pixel width
 # 
-ffmpeg -ss $2 -t $3 -i $1 -filter_complex "[0:v] fps=$4,scale=480:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" output.gif
+ffmpeg -ss $2 -t $3 -i $1 -filter_complex "[0:v] fps=$4,scale=$5:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" output.gif
